01027f08ca176b57a603846a31c2ecb2fee2c24f
Laszlo Velinszky <laszlo.velinszky@meltwater.com>
2018-10-22 13:52:24 -0700

    Added auto_cardinality option and more tests.

--- jugri/pandify.py
+++ jugri/pandify.py
@@ -25,18 +25,33 @@ def _flatten(d, parent_key='', sep='_'):
     return dict(items)
 
 
-def toDF(gremlin_traversal, keep_first_only=None, key_value_pairs=False, flatten_dict=True):
-    # type: (bool, bool, bool) -> pd.DataFrame
+def _get_singular(value):
+    if type(value) is list:
+        lv = len(value)
+        if  lv == 1:
+            return value[0]
+        elif lv == 0:
+            None
+        else:
+            return value
+    else:
+        return value
+
+
+def toDF(gremlin_traversal, keep_first_only=None, key_value_pairs=False, flatten_dict=True, auto_cardinality=True):
+    # type: (bool, bool, bool, bool) -> pd.DataFrame
     """
     Converts a Gremlin Traversal to a Pandas DataFrame. It expects a traversal or a list of traversal results.
     :param gremlin_traversal: A gremlinpython graph traversal (e.g. g.V().limit(5))
             or a list (e.g. g.V().limit(5).toList())
-    :param keep_first_only: Treats every field as a singular field. Only first values are kept.
+    :param keep_first_only: Deprecated since v0.3. Conversion is automatic and can be turned off
+            using the auto_cardinality option.
     :param key_value_pairs: Set it to True when a map is returned. The DataFrame will have only two columns: the key and
             the values.
     :param flatten_dict: Set it to True if you have nested fields in your results. The field name will automatically
             become the "." concatenated hierarchy of the names (e.g. start.date.month, end.date.year, etc.)
-    :param debug_info: Set it to True if you want to print some additional debug info. DEPRECATED
+    :param auto_cardinality: Set it to False for better performance. Singe element arrays will be automatically
+            converted into the first element.
     :return: Pandas DataFrame
     """
     if type(gremlin_traversal) is GraphTraversal:
@@ -44,11 +59,13 @@ def toDF(gremlin_traversal, keep_first_only=None, key_value_pairs=False, flatten
     if len(gremlin_traversal) == 0:
         return pd.DataFrame()
 
+    if keep_first_only is not None:
+        raise DeprecationWarning("""keep_first_only is deprecated since v0.3. 
+        Conversion is automatic and can be turned off using the auto_cardinality option.""")
+
     logger.debug("Type of first element: {}".format(type(gremlin_traversal[0])))
 
     if type(gremlin_traversal[0]) is dict:
-        if keep_first_only is None:
-            keep_first_only = True
         if flatten_dict:
             gremlin_traversal = [_flatten(_, sep='.') for _ in gremlin_traversal]
         if key_value_pairs:
@@ -57,18 +74,12 @@ def toDF(gremlin_traversal, keep_first_only=None, key_value_pairs=False, flatten
         else:
             df = pd.DataFrame(gremlin_traversal)
     elif isinstance(gremlin_traversal[0], Element):
-        if keep_first_only is None:
-            keep_first_only = False
         df = pd.DataFrame([_.__dict__ for _ in gremlin_traversal])
     elif type(gremlin_traversal[0]) is Path:
-        if keep_first_only is None:
-            keep_first_only = False
         df = pd.DataFrame([dict(enumerate(_)) for _ in gremlin_traversal])
     else:
-        if keep_first_only is None:
-            keep_first_only = False
         df = pd.DataFrame(gremlin_traversal)
-    if keep_first_only:
-        return df.applymap(lambda x: x[0] if type(x) is list else x)
+    if auto_cardinality:
+        return df.applymap(_get_singular)
     else:
         return df
