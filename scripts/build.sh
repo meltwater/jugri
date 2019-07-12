b0f87b0b0be4d9d7a99368684d4fc9a1f82ce72f
Laszlo Velinszky <laszlo.velinszky@meltwater.com>
2019-07-12 12:38:15 -0700

    Added build script and updated version and changelog

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+python setup.py clean --quiet
+python setup.py cython --quiet
 python setup.py sdist bdist_wheel
+
+python3 setup.py clean --quiet
+python3 setup.py cython --quiet
 python3 setup.py sdist bdist_wheel
